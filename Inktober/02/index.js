aa54cac527e1787ac34205fe6f65849e022c2f96
Steven Chuinard <steven.chuinard@outlook.fr>
2023-10-02 23:27:21 +0200

    2023.10.02

--- Inktober/02/index.js
+++ Inktober/02/index.js
@@ -4,66 +4,131 @@ var module = angular.module("Inktober", ["ngMaterial"]);
 
 module.controller("Controller", Controller);
 
+/**
+ * 
+ * @param {angular.IScope} $scope
+ */
 function Controller($scope)
 {
     var delai = 10;
     var canvas = document.getElementById("Jeu");
     /**@type {Spider} */
-    var spiders = [new Spider(80, 80), new Spider(150, 150)];
-
-    spiders[0].Color = "Red";
-    spiders[1].Color = "Green";
+    var spiders = [];
+    var timeStart = null;
 
     this.Init = function ()
     {
-        $scope.Cursor = new Point(1000, 400);
+        canvas.height = canvas.clientHeight;
+        canvas.width = canvas.clientWidth;
+        $scope.Cursor = new Point(window.innerWidth / 2, window.innerHeight / 2);
+        $scope.Game = false;
 
-        window.requestAnimationFrame(PlaceForme);
+        $scope.StartGame();
+
+        window.requestAnimationFrame(GameLoop);
     }
 
     $scope.Mouse = function (ev)
     {
-
+        $scope.Cursor.X = ev.offsetX;
+        $scope.Cursor.Y = ev.offsetY;
     }
 
-    function PlaceForme()
+    $scope.StartGame = function ()
     {
-        var size = 15;
-        const time = new Date();
+        spiders = [];
+        $scope.Game = true;
+        timeStart = new Date();
 
-        /**@type {CanvasRenderingContext2D} */
-        const ctx = canvas.getContext("2d");
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.save();
-
-        ctx.translate($scope.Cursor.X, $scope.Cursor.Y);
-        ctx.beginPath();
-        ctx.arc(0, 0, 5, 0, Math.PI * 2);
-        ctx.fill();
-
-        ctx.translate(-$scope.Cursor.X, -$scope.Cursor.Y);
-
-        for (var spider of spiders)
+        for (var i = 0; i < 20; i++)
         {
-            spider.Move($scope.Cursor);
-
-            ctx.translate(spider.X, spider.Y);
-            //ctx.rotate((Math.PI / 180) * 1);
-            ctx.rotate(spider.Rotate);
-            ctx.beginPath();
-            ctx.moveTo(0, -size);
-            ctx.lineTo(size * 2 / 5, size / 2);
-            ctx.lineTo(-(size * 2 / 5), size / 2);
-            ctx.fill();
-
-            ctx.rotate(-spider.Rotate);
-            ctx.translate(-spider.X, -spider.Y);
+            AddSpider(false);
         }
 
-        ctx.restore();
+        AddSpider();
+    }
 
-        window.requestAnimationFrame(PlaceForme);
+    function GameLoop()
+    {
+        if ($scope.Game)
+        {
+            var size = 15;
+            var duree = new Date(new Date() - timeStart);
+            $scope.$apply(() => $scope.Duree = duree.getMinutes() * 60 + duree.getSeconds() + duree.getMilliseconds() / 1000);
+
+            var timer = duree.getMinutes() * 60 + duree.getSeconds() + duree.getMilliseconds() / 1000;
+
+            /**@type {CanvasRenderingContext2D} */
+            const ctx = canvas.getContext("2d");
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.save();
+
+            ctx.translate($scope.Cursor.X, $scope.Cursor.Y);
+
+            if (duree.getMinutes() > 0)
+                ctx.fillStyle = "red";
+            else
+                ctx.fillStyle = "black";
+
+            ctx.beginPath();
+            ctx.arc(0, 0, 5, 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.translate(-$scope.Cursor.X, -$scope.Cursor.Y);
+
+            for (var spider of spiders)
+            {
+                spider.Move($scope.Cursor);
+
+                if (spider.Speed == 2)
+                    ctx.fillStyle = "black";
+                else
+                    ctx.fillStyle = "red";
+
+                ctx.translate(spider.X, spider.Y);
+                //ctx.rotate((Math.PI / 180) * 1);
+                ctx.rotate(spider.Rotate);
+
+                ctx.drawImage(spider.GatImage(), -(25 / 2), -(27 / 2), 25, 27);
+                //ctx.beginPath();
+                //ctx.moveTo(0, -size);
+                //ctx.lineTo(size * 2 / 5, size / 2);
+                //ctx.lineTo(-(size * 2 / 5), size / 2);
+                //ctx.fill();
+
+                ctx.rotate(-spider.Rotate);
+                ctx.translate(-spider.X, -spider.Y);
+            }
+
+
+        }
+
+        //ctx.restore();
+
+        window.requestAnimationFrame(GameLoop);
+    }
+
+    function StopGame()
+    {
+        $scope.Game = false;
+        var duree = new Date(new Date() - timeStart);
+
+        $scope.Duree = duree.getMinutes() * 60 + duree.getSeconds() + duree.getMilliseconds() / 1000;
+    }
+
+    function AddSpider(timer = true)
+    {
+        if ($scope.Game)
+        {
+            var screenSize = new Point(canvas.width, canvas.height);
+            var duree = new Date(new Date() - timeStart);
+
+            spiders.push(new Spider(screenSize, duree.getMinutes() > 0, StopGame));
+
+            if (timer)
+                setTimeout(AddSpider, delai * 10);
+        }
     }
 }
 
@@ -83,6 +148,41 @@ class Spider extends Point
 {
     Rotate = 0;
     Speed = 2;
+    Toucher = null;
+    SpiderBlack = new Image();
+    SpiderRed = new Image();
+
+    /**
+     * 
+     * @param {Point} screenSize
+     */
+    constructor(screenSize, speedUp, toucherCallback)
+    {
+        var pos = Math.random() * 2 * Math.PI;
+
+        var x = Math.cos(pos - (Math.PI / 2)) * screenSize.X * 3 / 4 + screenSize.X / 2;
+        var y = Math.sin(pos - (Math.PI / 2)) * screenSize.Y * 3 / 4 + screenSize.Y / 2;
+
+        super(x, y);
+
+        this.Toucher = toucherCallback;
+
+        this.SpiderBlack.src = "Images/Spider_One.png";
+        this.SpiderRed.src = "Images/SpiderRed.png";
+
+        if (speedUp)
+            this.Speed *= 2;
+        else
+            setTimeout(() => this.Speed *= 2, 10000);
+    }
+
+    GatImage()
+    {
+        if (this.Speed == 2)
+            return this.SpiderBlack;
+        else
+            return this.SpiderRed;
+    }
 
     /**
      * 
@@ -90,9 +190,18 @@ class Spider extends Point
      */
     Move(curseur)
     {
-        SetRotation(curseur);
+        this.SetRotation(curseur);
 
+        var distance = Math.sqrt(Math.pow(curseur.X - this.X, 2) + Math.pow(curseur.Y - this.Y, 2));
 
+        if (distance < this.Speed)
+            this.Toucher();
+
+        var deltaX = Math.cos(this.Rotate + (Math.PI / 2)) * this.Speed;
+        var deltaY = Math.sin(this.Rotate + (Math.PI / 2)) * this.Speed;
+
+        this.X += deltaX;
+        this.Y += deltaY;
     }
 
     /**
@@ -109,11 +218,16 @@ class Spider extends Point
         const angleEnRadians = Math.atan2(deltaY, deltaX);
 
         // Conversion de l'angle en radians en degrés
-        const angleEnDegres = (angleEnRadians * 180) / Math.PI;
+        //const angleEnDegres = (angleEnRadians * 180) / Math.PI;
 
         // L'angle calculé est par rapport à l'axe x positif, ajustons-le pour qu'il soit par rapport à l'axe y positif
-        const angleFinal = angleEnDegres - 90;
+        const angleFinal = angleEnRadians - (Math.PI / 2);
 
         this.Rotate = angleFinal;
+    }
+
+    getRandomInt(max)
+    {
+        return Math.floor(Math.random() * max);
     }
 }
