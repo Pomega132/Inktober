4769c6673314b6e09b8e5b60975ba29f8bf760a1
Steven Chuinard <steven.chuinard@gmail.com>
2025-09-29 14:59:25 +0200

    Ajout de thèmes et mise à jour du calendrier 2025
    - Création de la classe statique `Listes` avec `THEMES_2025`.
    - Ajout de styles CSS pour le calendrier dans `Cal2025.razor.css`.
    - Nouvelle page `/2025/1` dans `1.razor`.
    - Modification de `MainLayout.razor` pour améliorer la mise en page.
    - Intégration des thèmes dans le calendrier de `Cal2025.razor`.
    - Ajout de styles dans `app.css` pour gérer la hauteur et l'opacité.

---
+++ Webtober2025/Webtober2025.Client/Models/Listes.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Webtober2025.Client.Models
+{
+	public static class Listes
+	{
+		public static List<string> THEMES_2025 { get; } =
+        [
+            "MOUSTACHE",
+			"TISSER",
+			"COURONNE",
+			"TROUBLE",
+			"CERF",
+			"PERCER",
+			"ÉTOILE DE MER",
+			"IMPRUDENT",
+			"LOURD",
+			"BALAYER",
+			"PIQûre",
+			"DÉCHIRÉ",
+			"BOIRE",
+			"TRONC",
+			"EN LAMBEAUX",
+			"BOURDE",
+			"ORNÉ",
+			"ACCORD",
+			"ARCTIQUE",
+			"RIVAUX",
+			"EXPLOSION",
+			"BOUTON",
+			"LUCIOLE",
+			"TAPAGEUR",
+			"ENFER",
+			"DÉROUTANT",
+			"OIGNON",
+			"SQUELLETIQUE",
+			"LEÇON",
+			"VIDE",
+			"RECOMPENSE",
+		];
+	}
+}
