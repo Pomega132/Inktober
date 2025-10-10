2713549038ce6cfa9bffa48102cc090d226580ad
Steven <steven.chuinard@gmail.com>
2025-10-10 17:54:47 +0200

    Ajout de la logique de jeu et des ressources graphiques
    - Création de la classe `Point` en C# pour représenter un point 2D avec des méthodes de comparaison et de hachage.
    - Introduction de `GameRender.js` pour gérer l'initialisation du jeu, le rendu des éléments (joueur, pneus, corbeaux) et la gestion des textures.
    - Ajout et modification de fichiers PNG (`0.png`, `1.png`, `2.png`, `3.png`, `4.png`) pour le rendu graphique.
    - Corrections de types dans `06.razor` pour assurer la conversion correcte des coordonnées.
    - Mise à jour de `launchSettings.json` pour changer l'URL de lancement de l'application.
    - Modification de `Webtober2025.csproj` pour inclure un nouveau dossier d'images pour octobre 2025.
    - Ajustements dans `10.razor`, ajout de références et implémentation de `IAsyncDisposable` pour la gestion des ressources JavaScript.

---
+++ Webtober2025/Webtober2025.Client/Models/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Webtober2025.Client.Models
+{
+    public class Point(double x, double y)
+    {
+        public double X { get; set; } = x;
+        public double Y { get; set; } = y;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Point p) return false;
+            return this == p;
+        }
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+
+        public static bool operator ==(Point a, Point b) => a.X == b.X && a.Y == b.Y;
+        public static bool operator !=(Point a, Point b) => !(a == b);
+    }
+}
---
+++ Webtober2025/Webtober2025/wwwroot/Scripts/10/GameRender.js
@@ -0,0 +1,133 @@
+﻿/** @type {GameStateInfo} */
+export var GameState = GameState || {};
+
+export function InitGame()
+{
+    GameState = new GameStateInfo();
+}
+
+/**
+ * 
+ * @param {Rect} player
+ * @param {Array<Rect>} tires
+ * @param {Array<Crow>} crows
+ */
+export function RenderGame(player, tires, crows)
+{
+    // Effacer le canvas.
+    GameState.Context.clearRect(0, 0, GameState.Canvas.width, GameState.Canvas.height);
+
+    // Afficher un rectangle blanc pour le joueur.
+    GameState.Context.fillStyle = "white";
+    GameState.Context.fillRect(player.position.x, player.position.y, player.size.x, player.size.y);
+
+    // Afficher des ronds blancs avec un contour en dégradé bleu clair pour les tires.
+    for (var tire of tires)
+    {
+        GameState.Context.fillStyle = "white";
+        GameState.Context.strokeStyle = "lightblue";
+        GameState.Context.lineWidth = 5;
+        GameState.Context.beginPath();
+        GameState.Context.arc(tire.position.x, tire.position.y, tire.size.x / 2, 0, Math.PI * 2);
+        GameState.Context.fill();
+        GameState.Context.stroke();
+    }
+
+    // Afficher les crows.
+    for (var crow of crows)
+    {
+        var textureCat = "";
+        switch (crow.etat)
+        {
+            case E_CROW_STATE.FLY:
+                textureCat = "Fly";
+                break;
+            case E_CROW_STATE.ATTAQUE:
+                textureCat = "Attaque";
+                break;
+            case E_CROW_STATE.GET_DAMAGE:
+                textureCat = "GetDamage";
+                break;
+            case E_CROW_STATE.DEAD:
+                textureCat = "Dead";
+                break;
+        }
+        var texture = GameState.Textures.Texture[textureCat][Math.floor(crow.frame) % GameState.Textures.TextureIds[textureCat].length];
+        GameState.Context.drawImage(texture, crow.position.x, crow.position.y, crow.size.x, crow.size.y);
+    }
+}
+
+export class Textures
+{
+    Texture = {};
+    TextureIds = {
+        Attaque: [0, 1, 2, 3, 4],
+        Dead: [0, 1, 2, 3, 4],
+        Fly: [0, 1, 2, 3, 4],
+        GetDamage: [0, 1, 2, 3, 4],
+    };
+
+    constructor()
+    {
+        for (var textureCat in this.TextureIds)
+        {
+            this.Texture[textureCat] = {};
+
+            for (var texture of this.TextureIds[textureCat])
+            {
+                var img = new Image();
+                img.src = `/Images/2025/10/${textureCat}/${texture}.png`;
+                this.Texture[textureCat][texture] = img;
+            }
+        }
+    }
+}
+
+export class GameStateInfo
+{
+
+    constructor()
+    {
+        /** @type {HTMLCanvasElement} */
+        this.Canvas = document.getElementById("game");
+        this.Context = this.Canvas.getContext("2d");
+        this.Textures = new Textures();
+    }
+}
+
+export class Rect
+{
+    constructor()
+    {
+        this.position = new Point(0, 0);
+        this.size = new Point(100, 100);
+    }
+}
+
+export class Crow extends Rect
+{
+    constructor()
+    {
+        super();
+        /** @type {E_CROW_STATE} */
+        this.etat = E_CROW_STATE.FLY;
+        this.frame = 0;
+    }
+}
+
+/** @enum {number} */
+export const E_CROW_STATE = {
+    FLY: 0,
+    ATTAQUE: 1,
+    GET_DAMAGE: 2,
+    DEAD: 3
+}
+
+export class Point
+{
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
