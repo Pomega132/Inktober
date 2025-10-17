8f503f51da1ef79dc9c6dc88f0211827d452ba9e
Steven <steven.chuinard@gmail.com>
2025-10-17 19:47:49 +0200

    Ajout de la gestion des contrôles de jeu
    Implémentation de l'interface `IKeyState` pour gérer l'état des touches pressées, avec des méthodes pour ajouter et supprimer des pressions de touches. Introduction des classes `KeyControles` et `KeyControle` pour gérer les liaisons de touches.
    
    Ajout de classes dans `GameRender.js` pour gérer l'état du jeu et les textures, y compris `GameStateInfo`, `Textures`, `Rect` et `Point`.
    
    Mise à jour de `14.razor` pour intégrer l'interface `IKeyState` et gérer les événements de pression de touches sur un canevas HTML.
    
    Modification des valeurs booléennes dans `Listes.cs` pour certains contrôles.
    
    Mise à jour de la version de l'assemblage dans `Webtober2025.csproj` de `1.16.0.0` à `1.17.0.0`.

---
+++ Webtober2025/Webtober2025.Client/Models/IKeyState.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using static Webtober2025.Client.Models.IKeyState;
+using System.Runtime.ConstrainedExecution;
+
+namespace Webtober2025.Client.Models
+{
+    public interface IKeyState
+    {
+        List<E_Controle> KeysPressed { get; }
+        KeyControles Controles { get; }
+
+        public void AddKeyPress(KeyboardEventArgs args)
+        {
+            E_Controle? controle = Controles.GetControle(args.Code);
+            if (controle == null)
+                return;
+
+            //OnKeyDown?.Invoke(controle.Value);
+
+            if (!KeysPressed.Contains(controle.Value))
+                KeysPressed.Add(controle.Value);
+        }
+
+        public void RemoveKeyPress(KeyboardEventArgs args)
+        {
+            E_Controle? controle = Controles.GetControle(args.Code);
+
+            if (controle == null)
+                return;
+
+            //OnKeyUp?.Invoke(controle.Value);
+
+            if (KeysPressed.Contains(controle.Value))
+                KeysPressed.Remove(controle.Value);
+        }
+
+        public enum E_Controle
+        {
+            LEFT,
+            RIGHT,
+            UP,
+            DOWN,
+            JUMP,
+            ACTION,
+            PAUSE,
+            DEBUG
+        }
+
+        public class KeyControles : List<KeyControle>
+        {
+            public E_Controle? GetControle(string key)
+            {
+                foreach (var controle in this)
+                {
+                    if (controle.KeysBinding.Contains(key))
+                    {
+                        return controle.C;
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public class KeyControle
+        {
+            public E_Controle C { get; set; }
+            public List<string> KeysBinding { get; set; } = new List<string>();
+        }
+    }
+}
---
+++ Webtober2025/Webtober2025/wwwroot/Scripts/14/GameRender.js
@@ -0,0 +1,51 @@
+﻿/** @type {GameStateInfo} */
+export var GameState = GameState || {};
+
+export function InitGame() {
+    GameState = new GameStateInfo();
+}
+
+export function RenderGame() {
+    
+}
+
+export class Textures {
+    Texture = {};
+    TextureIds = [
+        "Bee/0",
+        "Bee/1",
+        "Pique"
+    ];
+
+    constructor() {
+        for (var texture of this.TextureIds) {
+            var img = new Image();
+            img.src = `/Images/2025/11/${texture}.png`;
+            this.Texture[texture] = img;
+        }
+    }
+}
+
+export class GameStateInfo {
+
+    constructor() {
+        /** @type {HTMLCanvasElement} */
+        this.Canvas = document.getElementById("game");
+        this.Context = this.Canvas.getContext("2d");
+        this.Textures = new Textures();
+    }
+}
+
+export class Rect {
+    constructor() {
+        this.position = new Point(0, 0);
+        this.size = new Point(100, 100);
+    }
+}
+
+export class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
